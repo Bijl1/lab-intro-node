d8aeeb18cdb162ef79822dcab8568be53b7d80d5
Jonathan Bijl <sannoayo@gmail.com>
2023-05-06 09:01:10 -0400

    Iteration 1-3 solved

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
 
   max() {}
 
